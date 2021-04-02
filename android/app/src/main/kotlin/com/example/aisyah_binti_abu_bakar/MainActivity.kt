91ea7af502b6a0891510f720f5cb8e669978ddf5
Nahl <nahlultra@gmail.com>
2021-04-02 15:07:23 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/aisyah_binti_abu_bakar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aisyah_binti_abu_bakar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/home.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: DefaultTextStyle(
+          style: GoogleFonts.workSans(color: Colors.black),
+          child: ListView(
+            padding: EdgeInsets.only(right: 24, left: 24, top: 64, bottom: 0),
+            children: [
+              Text("Aisyah binti Abu Bakar",
+                  style: GoogleFonts.workSans(
+                      fontWeight: FontWeight.bold, fontSize: 42)),
+              Text("Biografi", style: TextStyle(fontSize: 24)),
+              SizedBox(height: 64),
+              Text(
+                  '''Ummul Mukminin Aisyah radhiallahu ‘anha adalah satu-satunya wanita yang dinikahi Rasulullah shallallahu ‘alaihi wa sallam dengan status gadis. Beliau yang paling muda dalam usia pernikahan. Dan istri nabi yang paling banyak meriwayatkan hadits.
+
+Nama dan Nasabnya
+
+Beliau adalah Ummul Mukminin Aisyah binti Abu Bakr ash-Shiddiq. Merupakan istri Nabi shallallahu ‘alaihi wa sallam yang paling masyhur. Beliau lahir pada tahun 9 Sebelum Hijrah. Kun-yahnya adalah Ummu Abdullah dan dilaqobi dengan ash-Shidqiyah. Dan kita umat Nabi Muhammad mengenalnya dengan ummul mukminin (ibunya orang-orang beriman). Nabi memanggilnya dengan Humaira, karena putihnya kulit beliau. Ibunya adalah Ummu Ruman bin Amir bin Uwaimir al-Kinaniyah radhiallahu ‘anha.
+
+Di Masa Jahiliyah
+
+Ummul Mukminin Aisyah radhiallahu ‘anha lahir setelah risalah Islam diturunkan. Artinya, beliau tidak sempat mengalami fase jahiliyah dalam kehidupannya. Beliau terlahir dalam sebagai muslimah karena kedua orang tuanya telah memeluk Islam. Diriwayatkan oleh al-Bukhari dan Muslim, dari Urwah bin az-Zubair bahwa Aisyah istri Nabi shallallahu ‘alaihi wa sallam berkata,
+
+لم أعقل أبوي إلا وهما يدينان الدين
+
+“Aku belumlah berusia baligh ketika kedua orang tuaku sudah memeluk Islam.”
+
+Menikah
+
+Aisyah dilahirkan di rumah yang penuh cahaya keimanan. Kedua orang tuanya yang merupakan sahabat terbaik Rasulullah shallallahu ‘alaihi wa sallam adalah pembimbingnya. Ia tumbuh dengan bimbingan agama yang mulia dan pengajaran budi pekerti yang luhur.
+
+Rasulullah shallallahu ‘alaihi wa sallam melamar Aisyah saat ia berusia tujuh tahun. Dan menikahinya saat berusia sembilan tahun. Sebagian orang mungkin terheran-heran dengan usia Aisyah saat dinikahi Rasulullah. Keheranan tersebut dikarenakan ia mengukur kondisi zaman dulu dengan tradisi yang berlaku di zaman sekarang. Padahal Abu Jahal sekalipun tidak pernah mencela pernikahan ini. Kita tahu sendiri, Abu Jahal akan memanfaatkan setiap peluang untuk mencari cela yang ada pada nabi kita Muhammad shallallahu ‘alaihi wa sallam.
+
+Karena usianya yang sangat muda, tidak heran setelah menikah dengan Rasulullah kita mendapati riwayat-riwayat yang menceritakan bahwa Aisyah masih suka bermain-main layaknya anak kecil. Diriwayatkan dari Aisyah, ia berkata, “Rasulullah pernah menemuiku saat aku bermanin dengan anak-anak perempuan. ‘Apa ini hai Aisyah?” tanya beliau. Kujawab, ‘Ini adalah kudanya Sulaiman’. Beliau pun tertawa.”
+
+Rasulullah shallallahu ‘alaihi wa sallam sangat mencintai istrinya yang masih belia ini. Beliau berpesan kepada ibu Aisyah, Ummu Ruman, “Hai Ummu Ruman, aku menitipkan Aisyah agar engkau membimbingnya dengan baik. Dan jagalah untukku apa yang ada padanya.” Rasulullah sangat senang berjumpa dengannya. Beban yang beliau rasa menjadi hilang dengan menemuinya.
+
+Setelah hijrah ke Madinah, Rasulullah shallallahu ‘alaihi wa sallam melangsungkan resepsi pernikahan dengan Aisyah. Beliau mulai berumah tangga dengannya. Kota Madinah pun turut merasa bahagia dengan kebahagiaan keduanya. Setelah sebelumnya mereka bergembira dengan kemenangan di Perang Badar. Pernikahan ini dilaksanakan di bulan Syawwal tahun ke-2 H. Sejak saat itulah, Aisyah hidup bersama suaminya, mebina rumah tangga bersama suami terbaik dunia dan akhirat.
+
+Ilmu Yang Luas
+
+Atha bin Abi Rayah berkata, “Aisyah radhiallahu ‘anha adalah orang yang paling fakih (paham agama). Seorang yang paling baik pandangannya dalam permasalahan umat.”
+
+Abu Musa al-Asy’ari radhiallahu ‘anhu berkata, “Tidaklah kami para sahabat Muhammad shallallahu ‘alaihi wa sallam bingung dalam suatu hadits, niscaya kami bertanya kepada Aisyah, dan pasti kami dapati pengetahuan padanya tentang hal itu.”
+
+Seorang tokoh tabi’in dan keponakan Aisyah, Urwah bin az-Zubair berkata, “Aku tak pernah melihat seseorang yang paling tahu tentang halal dan haram. Tentang ilmu, syair, dan ilmu pengobatan melebihi Aisyah Ummul Mukminin radhiallahu ‘anha.”
+
+Kedudukan Yang Istimewa
+
+Diriwayatkan oleh al-Bukhari dalam Shahih nya, dari Abu Musa al-Asy’ari radhiallahu ‘anha, Rasulullah shallallahu ‘alaihi wa sallam bersabda,
+
+كَـمُـلَ مِنَ الرِّجَالِ كَثِيرٌ، وَلَمْ يَكْمُلْ مِنَ النِّسَاءِ غَيْرُ مَرْيَمَ بِنْتِ عِمْرَانَ، وَآسِيَةَ امْرَأَةِ فِرْعَوْنَ، وَإِنَّ فَضْلَ ‏عَائِشَةَ عَلَى النِّسَاءِ كَفَضْلِ الثَّرِيدِ عَلَى سَائِرِ الطَّعَامِ
+
+”Lelaki yang sempurna jumlahnya banyak. Dan tidak ada wanita yang sempurna selain ‎Maryam bintu Imran dan Asiyah istri Firaun. Dan keutamaan Aisyah dibandingkan ‎wanita lainnya, sebagaimana keutamaan ats-Tsarid dibandingkan makanan lainnya.” ‎‎(HR. Bukhari 5418 dan Muslim 2431).
+
+Diriwayatkan oleh Imam Muslim dalam Shahihnya dari Aisyah radhiallahu ‘anha, ia berkata,
+
+“كان الناس ‏‏يتحرّون ‏بهداياهم يوم ‏عائشة؛ ‏يبتغون ‏بذلك مرضاة رسول الله ‏صلى الله عليه وسلم
+
+“Orang-orang berusaha memberikan hadiah terbaik di hari pernikahan Aisyah. Dengan hadiah (kado pernikahan) itu mereka berharap keridhaan Rasulullah shallallahu ‘alaihi wa sallam.”
+
+Diriwayatkan oleh al-Bukhari dan Muslim dalam Shahih keduanya dari Abu Salamah, Aisyah radhiallahu ‘anha bercerita bahwa Rasulullah shallallahu ‘alaihi wa sallam suatu hari bersabda,
+
+يا عائش هذا جبريل يقرئك السلام. قلت: وعليه السلام ورحمة الله، قالت: وهو يرى ما لا نرى.
+
+“Yaa A-isy, ini Jibril mengucapkan salam kepadamu”. Aku (Aisyah) berkata, “Wa’alaihis-salaam warahmatullaah”. ‘Aaisyah berkata, “Jibril itu melihat sesuatu yang tidak kita lihat.”
+
+Dari Masruq, ia berkata, “Aisyah bercerita radhiallahu ‘anha bercerita kepadaku, ia berkata, ‘Sungguh aku melihat Jibril berada di bagian depan rumahku ini sedang berada di atas kuda. Rasulullah shallallahu ‘alaihi wa sallam memanggilnya. Ketika ia masuk, aku berkata, ‘Hai Rasulullah, siapa orang yang aku lihat kau memanggilnya?’ Beliau bertanya, ‘Apakah kau melihatnya?’ ‘Iya’, jawabku. Beliau kembali bertanya, ‘Seperti siapa dia?’ Kujawab, ‘Mirip dengan Dihyah al-Kalbi’. Beliau bersabda, ‘Sungguh kau telah melihat kebaikan yang banyak. Itu adalah Jibril’. Aisyah berkata, ‘Jibril hanya mampir sebentar’. Kemudian Rasulullah bersabda, ‘Hei Aisyah, Jibril menyampaikan salam untukmu’. Aku (Aisyah) berkata, ‘Salam juga untuknya. Semoga Allah membalasnya dengan kebaikan’.”
+
+Rasulullah tahu persis perasaan dan ekspresinya Aisyah. Beliau tahu saat Aisyah senang. Dan beliau mengerti saat beliau sedang marah (ngambek). Dari Urwah bin Mas’ud, Aisyah radhiallahu ‘anha berkata, “Hai anak saudariku, Rasulullah shallallahu ‘alaihi wa sallam berkata padaku, “Tidak tersembunyi bagiku kapan kau marah dan kapan engkau ridha.” Aku bertanya, “Dari mana engkau tahu -demi ayah dan ibuku-?” Beliau bersabda, ‘Saat kau ridha, ketika bersumpah kau akan mengatakan ‘Demi Rabbnya Muhammad. Dan saat kau marah, kau akan katakan, ‘Demi Rabnya Ibrahim’. Aku berkata, ‘Anda benar wahai Rasulullah’.”
+
+Pendidik Umat
+
+Sebagaimana kita ketahui, Ummul Mukminin Aisyah radhiallahu ‘anha adalah salah seorang sahabat yang paling banyak meriwayatkan hadits Nabi shallallahu ‘alaihi wa sallam. Karena alasan itu pula banyak sahabat dan tabi’in yang berguru kepada beliau. Ada sekitar 299 orang sahabat dan tabi’in yang meriwayatkan hadits dari beliau.
+
+Setelah dua puluh tujuh tahun Madinah menjadi pusat pemerintahan kaum muslimin, perubahan pun terjadi. Ibu kota daulah islamiyah pindah ke Kufah. Namun, berubahnya keadaan tersebut tidak membuat pamor Madinah sebagai kota ilmu luntur. Para tokoh sahabat membuka majelis-majelis mereka di Madinah. Dan di antara majelis yang paling ramai disinggahi para pelajar adalah sebuah majelis yang berada di dekat rumah Nabi shallallahu ‘alaihi wa sallam. Dan pengajar di majelis tersebut adalah ibunda Aisyah radhiallahu ‘anha.
+
+Banyak ulama dan tokoh terkemuka lahir dari madrasah ibunda Aisyah radhiallahu ‘anha. Bahkan salah satu buku hadits yang paling terkenal di dunia Islam, Musnad Imam Ahmad, menghimpun sejumlah besar hadits yang diriwayatkan dari Aisyah. Riwayat dari beliau adalah riwayat terbanyak dalam buku tersebut.
+
+Setelah Rasulullah shalallahu ‘alaihi wa sallam wafat, beliau menjadi rujukan utama dalam hadits dan fatwa. Beliau menjadi figur yang diteladani dalam berbagai hal. Keadaan ini terus berlangsung selama masa khulafaur rasyidin hingga beliau wafat.
+
+Wafat
+
+Kapan Ibunda Aisyah radhiallahu ‘anha wafat?
+
+Diriwayatkan bahwa Ibunda Aisyah radhiallahu ‘anha wafat pada usia 85 tahun. Pada malam selesa tanggal 27 Ramadhan. Beliau meminta dikuburkan di malam hari. Dan dimakamkan di Baqi’ setelah shalat witir. Abu Hurairah radhiallahu ‘anhu memimpin shalat jenazahnya. Dan yang memasukkan jenazah beliau ke makamnya ada lima orang. Dua orang keponakan beliau: Abdullah dan Urwah, dua orang putra Zubair bin al-Awwam. Kemudian tiga orang cucu Abu Bakar ash-Shidiq: al-Qasim bin Muhammad dan Abdullah bin Muhammad bin Abu Bakar. Dan putra dari saudaranya: Abdullah bin Abdurrahman bin Abu Bakar, radhiallahu ‘anhum ajma’in.
+
+Sumber:
+– https://islamstory.com/ar/artical/74/أم_المؤمنين_عائشة_بنت_أبي_بكر
+
+
+
+Read more https://kisahmuslim.com/6164-ummul-mukminin-aisyah-binti-abu-bakar.html'''),
+              SizedBox(
+                height: 42,
+              ),
+              Wrap(
+                alignment: WrapAlignment.center,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Container(
+                      color: Color.fromRGBO(0, 0, 0, 0.04),
+                      padding: EdgeInsets.only(
+                          right: 12, left: 12, top: 8, bottom: 8),
+                      child: Text(
+                        "nahlmb.dev - 2020 - IDN Final Task\nsumber : kisahmuslim.com",
+                        style: TextStyle(color: Colors.black54),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 42,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
